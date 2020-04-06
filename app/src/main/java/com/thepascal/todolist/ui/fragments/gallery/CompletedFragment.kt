c78e97d0c0b525bce5563b86187c3572b4da82da
PascalCoder <pbaconqueror7@yahoo.com>
2020-04-06 14:59:58 -0400

    Added logic for completed tasks

--- app/src/main/java/com/thepascal/todolist/ui/fragments/gallery/CompletedFragment.kt
+++ app/src/main/java/com/thepascal/todolist/ui/fragments/gallery/CompletedFragment.kt
@@ -1,31 +1,56 @@
 package com.thepascal.todolist.ui.fragments.gallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.thepascal.todolist.R
+import com.thepascal.todolist.adapter.ToDoAdapter
+import com.thepascal.todolist.model.DataManager
 
-class CompletedFragment : Fragment() {
+class CompletedFragment : Fragment(), ToDoAdapter.OnTaskClickedListener {
 
-    private lateinit var completedViewModel: CompletedViewModel
+    private val completedViewModel: CompletedViewModel by lazy {
+        ViewModelProvider(this).get(CompletedViewModel::class.java)
+    }
+    private val completedTasksLayoutManager by lazy { LinearLayoutManager(context) }
+    private val completedTasksAdapter by lazy {
+        val adapter = ToDoAdapter(context!!, DataManager.completedTaskList)
+        adapter.setOnTaskClickedListener(this)
+        adapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        completedViewModel =
-            ViewModelProviders.of(this).get(CompletedViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         completedViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        val recyclerView = root.findViewById<RecyclerView?>(R.id.completedTasksRecyclerView)
+        recyclerView?.layoutManager = completedTasksLayoutManager
+        recyclerView?.adapter = completedTasksAdapter
+
         return root
+    }
+
+    override fun onCheckBoxClicked(itemPosition: Int) {
+        Log.d("Completed Fragment", "Item was clicked!")
+        completedViewModel.handleTaskCompleted(itemPosition)
+    }
+
+    override fun onDeleteImageClicked(itemPosition: Int) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/thepascal/todolist/ui/fragments/gallery/CompletedViewModel.kt
+++ app/src/main/java/com/thepascal/todolist/ui/fragments/gallery/CompletedViewModel.kt
@@ -3,6 +3,8 @@ package com.thepascal.todolist.ui.fragments.gallery
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.thepascal.todolist.model.DataManager
+import com.thepascal.todolist.model.TaskState
 
 class CompletedViewModel : ViewModel() {
 
@@ -10,4 +12,11 @@ class CompletedViewModel : ViewModel() {
         value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun handleTaskCompleted(itemPosition: Int) {
+        val toDoModel = DataManager.completedTaskList[itemPosition]
+        DataManager.completedTaskList[itemPosition].taskState = TaskState.ACTIVE
+        DataManager.activeTaskList.add(toDoModel)
+        DataManager.completedTaskList.removeAt(itemPosition)
+    }
 }
